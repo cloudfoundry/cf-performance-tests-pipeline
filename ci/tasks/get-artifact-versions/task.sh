39ef49ff9d2070db8b8e578146bb5434389fa3de
Jochen Ehret <jochen.ehret@sap.com>
2026-08-06 15:33:09 +0200

    Run cf-performance-tests for capi-release rc
    * run for each validated commit on the capi-release "ci-passed" branch
    * build capi dev release and upload to BOSH director before deploying cf
    * use latest released cf-deployment revision
    * use version from "capi-release-versions-app-runtime-interfaces" bucket to determine next capi-release version

--- ci/tasks/get-artifact-versions/task.sh
+++ ci/tasks/get-artifact-versions/task.sh
@@ -4,11 +4,14 @@ set -euo pipefail
 
 task_root="$(pwd)"
 cf_deployment_repo="${task_root}/cf-deployment"
+next_version_repo="${task_root}/next-version"
+capi_release_repo="${task_root}/capi-release-ci-passed"
 cf_versions_output="${task_root}/cf-versions"
 
 echo -e "\nGetting cf-deployment and capi versions..."
-cf_deployment_version="$(<"${cf_deployment_repo}"/cf-deployment.yml yq '.manifest_version' -r)"
-capi_version="$(<"${cf_deployment_repo}"/cf-deployment.yml grep -A 1 capi | grep version | cut -d ':' -f2 | sed 's/ //g')"
+cf_deployment_version="$(yq '.manifest_version' -r "${cf_deployment_repo}/cf-deployment.yml")"
+capi_release_short_ref="$(tr -d '[:space:]' < "${capi_release_repo}/.git/short_ref")"
+capi_version="$(tr -d '[:space:]' < "${next_version_repo}/version") (${capi_release_short_ref})"
 
 echo "cf_deployment_version: ${cf_deployment_version}" >> "${cf_versions_output}/cf_versions.yml"
 echo "capi_version: ${capi_version}" >> "${cf_versions_output}/cf_versions.yml"
